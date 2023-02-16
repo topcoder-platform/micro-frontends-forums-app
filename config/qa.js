be0d81232524fe7acd4e0ec305e0d0e3c3b0d4a3
Gunasekar-K <gunasekar.kesavalu@wipro.com>
2023-02-16 19:01:46 +0530

    QA changes

---
+++ config/qa.js
@@ -0,0 +1,7 @@
+module.exports = {
+  API: {
+    V3: "https://api.topcoder-qa.com/v3",
+    V5: "https://api.topcoder-qa.com/v5",
+  },
+  VANILLA_EMBED_JS: "https://vanilla.topcoder-qa.com/js/embed.js",
+};
